5e8ab4a705da4b45f2703cb4419af97fe8268918
Théophile <theoyoupy@orange.fr>
2024-08-08 19:32:31 +0200

    Delete useless debug logs for Player shooting script

--- Assets/Scripts/PlayerOne/Shoot.cs
+++ Assets/Scripts/PlayerOne/Shoot.cs
@@ -26,7 +26,6 @@ public class Shoot : MonoBehaviour
 
         if (rb == null)
         {
-            UnityEngine.Debug.LogError("The bullet prefab is missing a Rigidbody2D component.");
             return;
         }
 
@@ -35,7 +34,5 @@ public class Shoot : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; // Adjust for sprite orientation
         bullet.transform.rotation = Quaternion.Euler(0, 0, angle);
-
-        UnityEngine.Debug.Log($"Bullet Direction: {direction}, Bullet Velocity: {rb.velocity}, Bullet Rotation: {bullet.transform.rotation.eulerAngles}");
     }
 }
