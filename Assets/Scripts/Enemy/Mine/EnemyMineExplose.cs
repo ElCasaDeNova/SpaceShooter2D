36fd09f6663c5503ab6c1ad2c85ff3037a288278
Théophile <theoyoupy@orange.fr>
2024-08-22 17:16:10 +0200

    Add Mine Explosion Sound

--- Assets/Scripts/Enemy/Mine/EnemyMineExplose.cs
+++ Assets/Scripts/Enemy/Mine/EnemyMineExplose.cs
@@ -8,11 +8,34 @@ public class EnemyMineExplose : MonoBehaviour
     [SerializeField]
     private float damage;
 
+    [SerializeField]
+    private AudioSource audioSource;
+    private AudioClip explosionSound;
+
+    private void Start()
+    {
+        // Get the AudioSource component on this GameObject
+        explosionSound = audioSource.clip;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //TODO Shoot on Mine make it explode
+
         // If Player collides
         if (other.gameObject.TryGetComponent<PlayerOneHealth>(out PlayerOneHealth player))
         {
+            if (explosionSound != null)
+            {
+                // Create GameObject so the Cruiser Destruction doesn't block or wait for the explosion sound
+                GameObject tempAudioSource = new GameObject("TempAudioSource");
+                AudioSource tempSource = tempAudioSource.AddComponent<AudioSource>();
+                tempSource.clip = explosionSound;
+                tempSource.Play();
+
+                // Destroy GameObject when sound is done
+                Destroy(tempAudioSource, explosionSound.length);
+            }
             player.TakeDamage(damage);
             Destroy(mine);
         }
