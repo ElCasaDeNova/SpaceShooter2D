ee0b5da582a6da31380c43389f26e3a809aa87f3
Théophile <theoyoupy@orange.fr>
2024-08-28 11:41:44 +0200

    Create CruiserPooler

---
+++ Assets/Scripts/Utils/CruiserPooler.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CruiserPooler : MonoBehaviour
+{
+
+    public static CruiserPooler Instance;
+
+    public GameObject cruiserPrefab; // The cruiser prefab to pool
+    public int initialPoolSize = 20; // Initial pool size
+
+    private Queue<GameObject> cruiserPool;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        // Initialize the pool
+        cruiserPool = new Queue<GameObject>();
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject cruiser = Instantiate(cruiserPrefab);
+            cruiser.SetActive(false);
+            cruiserPool.Enqueue(cruiser);
+        }
+    }
+
+    public GameObject GetCruiser()
+    {
+        if (cruiserPool.Count > 0)
+        {
+            GameObject cruiser = cruiserPool.Dequeue();
+            cruiser.SetActive(true);
+            return cruiser;
+        }
+        else
+        {
+            // Optionally create a new cruiser if the pool is empty
+            GameObject cruiser = Instantiate(cruiserPrefab);
+            return cruiser;
+        }
+    }
+
+    public void ReturnCruiser(GameObject cruiser)
+    {
+        cruiser.SetActive(false);
+        cruiser.transform.SetParent(transform);
+        cruiserPool.Enqueue(cruiser);
+    }
+}
